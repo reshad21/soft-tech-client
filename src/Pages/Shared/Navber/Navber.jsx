9b2256ebe0b99e1ff7a0ebc1ccd3e158ff6ab59d
reshad21 <miakala7@gmail.com>
2023-01-05 12:32:59 +0600

    basic design complete

--- src/Pages/Shared/Navber/Navber.jsx
+++ src/Pages/Shared/Navber/Navber.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navber = () => {
     return (
         <div>
-            <h1>dasiy ui navber</h1>
+            <div className="navbar bg-base-100 px-6">
+                <div className="flex-1">
+                    <Link to='/' className="btn btn-ghost normal-case text-xl">SoftTech</Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><Link to='/'>Home</Link></li>
+                        <li tabIndex={0}>
+                            <a href='/'>
+                                Parent
+                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                            </a>
+                            <ul className="p-2 bg-base-100">
+                                <li><a href='/'>Submenu 1</a></li>
+                                <li><a href='/'>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><Link to='/admin'>Admin</Link></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Routes/Router/Router.jsx
+++ src/Routes/Router/Router.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router-dom";
 import Main from '../../Layout/Main';
+import Admin from '../../Pages/Admin/Admin';
 import Home from '../../Pages/Home/Home/Home';
 
 const router = createBrowserRouter([
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/admin',
+                element: <Admin></Admin>
             }
         ]
     },
