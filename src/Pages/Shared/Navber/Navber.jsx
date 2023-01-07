c927a3e9eea00b30a822e95101fa9a0801e8758a
reshad21 <miakala7@gmail.com>
2023-01-07 21:25:59 +0600

    all done

--- src/Pages/Shared/Navber/Navber.jsx
+++ src/Pages/Shared/Navber/Navber.jsx
@@ -10,18 +10,8 @@ const Navber = () => {
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal px-1">
-                        <li><Link to='/'>Home</Link></li>
-                        <li tabIndex={0}>
-                            <a href='/'>
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                            </a>
-                            <ul className="p-2 bg-base-100">
-                                <li><a href='/'>Submenu 1</a></li>
-                                <li><a href='/'>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><Link to='/admin'>Admin</Link></li>
+                        <li><Link to='/' className='btn btn-outline'>Home</Link></li>
+                        <li><Link to='/admin' className='btn btn-outline ml-4'>Admin</Link></li>
                     </ul>
                 </div>
             </div>
