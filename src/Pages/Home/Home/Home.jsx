55525f326f4fd96a72770b6e35408ea2b320e4f2
reshad21 <miakala7@gmail.com>
2023-01-07 11:35:53 +0600

    all data show home page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -23,6 +23,26 @@ const Home = () => {
     return (
         <div className="mx-auto max-w-7xl">
             <h1>Home page:{products.length}</h1>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 align-middle justify-center">
+                {
+                    products?.map((product) => {
+                        return (
+                            <div className="card card-compact w-96 bg-base-100 shadow-xl" key={product?._id}>
+                                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{product?.name}</h2>
+                                    <p>{product?.details}</p>
+                                    <div className="card-actions flex justify-between align-middle">
+                                        <label htmlFor="" className='font-bold'>Price: {product?.price}</label>
+                                        <button className="btn btn-primary">Update Price</button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
         </div>
     );
 };
